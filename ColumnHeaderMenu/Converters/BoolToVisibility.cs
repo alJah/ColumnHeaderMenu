173a6ab4791d165ed49a823a986d6a7ea6138a06
vss2 <jahgml2@gmail.com>
2022-05-03 11:00:55 +0700

    Add project files.

---
+++ ColumnHeaderMenu/Converters/BoolToVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Windows.Data;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ColumnHeaderMenu.Converters
+{
+    [ValueConversion(typeof(Boolean), typeof(Visibility))]
+    internal class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)value == Visibility.Visible) return true; return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value) return Visibility.Visible; return Visibility.Collapsed;
+        }
+    }
+}
